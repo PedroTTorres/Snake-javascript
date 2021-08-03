c28b8590048c16bfe3de8dfbccb517db5b84db37
Pedro <kpedro101@gmail.com>
2021-08-03 00:04:11 -0300

    Implementado a logica do jogo

--- script.js
+++ script.js
@@ -52,6 +52,13 @@ function iniciarJogo(){
     if (snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
     if (snake[0].y < 0 * box && direction == "down") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y ){
+            clearInterval(jogo);
+            alert("Game over")
+        }
+    }
+
     criarBG();
     criarSnake();
     drawFood();
@@ -63,6 +70,12 @@ function iniciarJogo(){
     if( direction == "left") snakex -= box;
     if( direction == "up") snakey += box;
     if( direction == "down") snakey -= box;
+
+    if (snakex == food.x && snakey == food.y){
+        snake.push(1);
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     snake.pop();
     
